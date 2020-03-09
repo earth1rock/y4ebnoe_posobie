dd671398ed9a611ea610da0c7986b0581b687923
Alexander.Vasiliev <smash3243@gmail.com>
2020-03-09 21:24:42 +0300

    Выделил отдельно footer и header, чтобы на всех страницах можно было подключить шапку и подвал

---
+++ footer.php
@@ -0,0 +1,25 @@
+<div id="footer">
+	<div class="navbar navbar-expand-lg navbar-dark bg-primary justify-content-center">
+
+    <ul class="navbar-nav ">
+      <li class="nav-item">
+        <a class="nav-link" href="#">Контакты</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">О нас</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">О проекте</a>
+      </li>
+    </ul>
+
+	</div>
+	</div>
+
+</div>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+<script src="js/bootstrap.js"></script>
+</div>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,59 +1,8 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>RewuEge</title>
-	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
-	<link rel="stylesheet" type="text/css" href="css/main.css">
-</head>
+<?php
+    //Подключение шапки
+    require_once("header.php");
+?>
 
-<body>
-
-	<div class="wrapper">
-
-	<div class="header">
-	<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-
-  <a class="navbar-brand" href="#">ПОСОБИЕ</a>
-  <!--кнопка, которая появляется при сжатии окна-->
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-  <!--Блок который будет скрывать навигацию -->
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-
-  	<div>
-    	<ul class="navbar-nav mr-auto">
-     		<li class="nav-item active">
-        		<a class="nav-link" href="#">Главная<span class="sr-only">(current)</span></a>
-      		</li>
-      		<li class="nav-item">
-       			<a class="nav-link" href="#">Личный кабинет</a>
-      		</li>
-      		<li class="nav-item">
-        		<a class="nav-link" href="#">Варианты</a>
-     		</li>
-      		<li class="nav-item">
-        		<a class="nav-link" href="#">Банк заданий</a>
-     		 </li>
-    </ul>
-	</div>
-	<div class="ml-auto">
-		<ul class="navbar-nav mr-auto">
-      <li class="nav-item">
-        <a class="nav-link" href="#">Авторизация<span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Регистрация</a>
-      </li>
-  </ul>
-	</div>
-
-</div>
-</nav>
-</div>
 
 <div class="content">
 
@@ -113,28 +62,7 @@
 
 </div>
 
-	<div id="footer">
-	<div class="navbar navbar-expand-lg navbar-dark bg-primary justify-content-center">
-
-    <ul class="navbar-nav ">
-      <li class="nav-item">
-        <a class="nav-link" href="#">Контакты</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">О нас</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">О проекте</a>
-      </li>
-    </ul>
-
-	</div>
-	</div>
-
-</div>
-
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
-<script src="js/bootstrap.js"></script>
-</div>
-</body>
-</html>
+	<?php
+    //Подключение подвала
+    require_once("footer.php");
+?>
