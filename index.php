9fe3913e8b7af1cee95f3eae2f648651961426ce
Alexander.Vasiliev <smash3243@gmail.com>
2020-03-09 20:56:44 +0300

    Первый макет главной страницы

---
+++ index.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>RewuEge</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+</head>
+
+<body>
+
+	<div class="wrapper">
+
+	<div class="header">
+	<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+
+  <a class="navbar-brand" href="#">ПОСОБИЕ</a>
+  <!--кнопка, которая появляется при сжатии окна-->
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <!--Блок который будет скрывать навигацию -->
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+  	<div>
+    	<ul class="navbar-nav mr-auto">
+     		<li class="nav-item active">
+        		<a class="nav-link" href="#">Главная<span class="sr-only">(current)</span></a>
+      		</li>
+      		<li class="nav-item">
+       			<a class="nav-link" href="#">Личный кабинет</a>
+      		</li>
+      		<li class="nav-item">
+        		<a class="nav-link" href="#">Варианты</a>
+     		</li>
+      		<li class="nav-item">
+        		<a class="nav-link" href="#">Банк заданий</a>
+     		 </li>
+    </ul>
+	</div>
+	<div class="ml-auto">
+		<ul class="navbar-nav mr-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="#">Авторизация<span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Регистрация</a>
+      </li>
+  </ul>
+	</div>
+
+</div>
+</nav>
+</div>
+
+<div class="content">
+
+	<div class="d-flex justify-content-center">
+			<div >
+				<h1>Главная страница</h1>
+				<br>
+				<br>
+			</div>
+	</div>
+
+<div class="aut">
+	<div class="container-fluid">
+
+	<div class="row">
+
+		<div class="inf col-lg-6 ">
+			<div class="tabl">
+			<p>
+			ИНФОРМАЦИЯ О САЙТЕ ИНФОРМАЦИЯ О САЙТЕ ИНФОРМАЦИЯ О САЙТЕ ИНФОРМАЦИЯ О САЙТЕ ИНФОРМАЦИЯ О САЙТЕ ИНФОРМАЦИЯ О САЙТЕ
+			ИНФОРМАЦИЯ О САЙТЕ ИНФОРМАЦИЯ О САЙТЕ ИНФОРМАЦИЯ О САЙТЕ ИНФОРМАЦИЯ О САЙТЕ ИНФОРМАЦИЯ О САЙТЕ ИНФОРМАЦИЯ О САЙТЕ
+			ИНФОРМАЦИЯ О САЙТЕ ИНФОРМАЦИЯ О САЙТЕ ИНФОРМАЦИЯ О САЙТЕ ИНФОРМАЦИЯ О САЙТЕ ИНФОРМАЦИЯ О САЙТЕ ИНФОРМАЦИЯ О САЙТЕ
+			ИНФОРМАЦИЯ О САЙТЕ ИНФОРМАЦИЯ О САЙТЕ ИНФОРМАЦИЯ О САЙТЕ ИНФОРМАЦИЯ О САЙТЕ ИНФОРМАЦИЯ О САЙТЕ ИНФОРМАЦИЯ О САЙТЕ
+			</p>
+		</div>
+		</div>
+
+		<div class="inf col-lg-5 offset-lg-1 ">
+			<div class="res">
+			<p>
+			ТАБЛИЦА РЕЗУЛЬТАТОВ ТАБЛИЦА РЕЗУЛЬТАТОВ ТАБЛИЦА РЕЗУЛЬТАТОВ ТАБЛИЦА РЕЗУЛЬТАТОВ ТАБЛИЦА РЕЗУЛЬТАТОВ ТАБЛИЦА РЕЗУЛЬТАТОВ
+			ТАБЛИЦА РЕЗУЛЬТАТОВ ТАБЛИЦА РЕЗУЛЬТАТОВ ТАБЛИЦА РЕЗУЛЬТАТОВ ТАБЛИЦА РЕЗУЛЬТАТОВ ТАБЛИЦА РЕЗУЛЬТАТОВ ТАБЛИЦА РЕЗУЛЬТАТОВ
+			ТАБЛИЦА РЕЗУЛЬТАТОВ ТАБЛИЦА РЕЗУЛЬТАТОВ ТАБЛИЦА РЕЗУЛЬТАТОВ ТАБЛИЦА РЕЗУЛЬТАТОВ ТАБЛИЦА РЕЗУЛЬТАТОВ ТАБЛИЦА РЕЗУЛЬТАТОВ
+			</p>
+		</div>
+		<br>
+
+
+			<div class="">
+
+				<a class="ychast btn btn-block" href="#">
+					<p>
+						ПРИНЯТЬ УЧАСТИЕ В ТЕСТИРОВАНИИ
+					</p>
+
+				</a>
+
+			</div>
+
+	</div>
+
+	</div>
+
+
+</div>
+</div>
+
+</div>
+
+	<div id="footer">
+	<div class="navbar navbar-expand-lg navbar-dark bg-primary justify-content-center">
+
+    <ul class="navbar-nav ">
+      <li class="nav-item">
+        <a class="nav-link" href="#">Контакты</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">О нас</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">О проекте</a>
+      </li>
+    </ul>
+
+	</div>
+	</div>
+
+</div>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+<script src="js/bootstrap.js"></script>
+</div>
+</body>
+</html>
