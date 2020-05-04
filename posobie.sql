3e2feca015367d50edc335f2b87c3bebdb5b676d
Alexander <smash3243@gmail.com>
2020-05-04 17:53:17 +0300

    Скрипт бд

--- posobie.sql
+++ posobie.sql
@@ -2,8 +2,8 @@
 -- version 4.9.0.1
 -- https://www.phpmyadmin.net/
 --
--- Хост: 192.168.1.3:3306
--- Время создания: Апр 30 2020 г., 18:33
+-- Хост: 192.168.1.6:3306
+-- Время создания: Май 04 2020 г., 17:51
 -- Версия сервера: 10.3.13-MariaDB-log
 -- Версия PHP: 7.1.32
 
@@ -23,6 +23,89 @@ SET time_zone = "+00:00";
 --
 CREATE DATABASE IF NOT EXISTS `posobie` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 USE `posobie`;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `answers`
+--
+
+DROP TABLE IF EXISTS `answers`;
+CREATE TABLE `answers` (
+  `id_answer` int(11) NOT NULL,
+  `id_question` int(11) NOT NULL,
+  `text` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `correct` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Дамп данных таблицы `answers`
+--
+
+INSERT INTO `answers` (`id_answer`, `id_question`, `text`, `correct`) VALUES
+(1, 5, 'Москва', 1),
+(2, 5, 'Ярославль', 0),
+(3, 1, 'Россия', 1),
+(4, 1, 'Канада', 0),
+(6, 1, 'США', 0),
+(7, 1, 'Китай', 0),
+(8, 2, 'США', 0),
+(9, 2, 'Россия', 0),
+(10, 2, 'Китай', 0),
+(11, 2, 'Люксембург', 1),
+(12, 3, 'Германия', 0),
+(13, 3, 'Франция', 0),
+(14, 3, 'Россия', 0),
+(15, 3, 'США', 1),
+(16, 4, 'Лада', 1),
+(17, 4, 'BMW', 0),
+(18, 4, 'Ford', 0),
+(19, 4, 'Audi', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `questions`
+--
+
+DROP TABLE IF EXISTS `questions`;
+CREATE TABLE `questions` (
+  `id_question` int(11) NOT NULL,
+  `text` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `id_test` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Дамп данных таблицы `questions`
+--
+
+INSERT INTO `questions` (`id_question`, `text`, `id_test`) VALUES
+(1, 'Самая большая по площади страна?', 1),
+(2, 'Самая маленькая страна?', 1),
+(3, 'Самая развитая страна?', 1),
+(4, 'Какая марка является русской?', 3),
+(5, 'Столица РФ?', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `tests`
+--
+
+DROP TABLE IF EXISTS `tests`;
+CREATE TABLE `tests` (
+  `id_test` int(11) NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Дамп данных таблицы `tests`
+--
+
+INSERT INTO `tests` (`id_test`, `name`) VALUES
+(1, 'Страны'),
+(2, 'Города'),
+(3, 'Автомобили');
 
 -- --------------------------------------------------------
 
@@ -55,6 +138,26 @@ INSERT INTO `users` (`id`, `first_name`, `middle_name`, `last_name`, `role`, `em
 --
 
 --
+-- Индексы таблицы `answers`
+--
+ALTER TABLE `answers`
+  ADD PRIMARY KEY (`id_answer`),
+  ADD KEY `id_question` (`id_question`);
+
+--
+-- Индексы таблицы `questions`
+--
+ALTER TABLE `questions`
+  ADD PRIMARY KEY (`id_question`),
+  ADD KEY `id_test` (`id_test`);
+
+--
+-- Индексы таблицы `tests`
+--
+ALTER TABLE `tests`
+  ADD PRIMARY KEY (`id_test`);
+
+--
 -- Индексы таблицы `users`
 --
 ALTER TABLE `users`
@@ -66,10 +169,44 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT для таблицы `answers`
+--
+ALTER TABLE `answers`
+  MODIFY `id_answer` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+
+--
+-- AUTO_INCREMENT для таблицы `questions`
+--
+ALTER TABLE `questions`
+  MODIFY `id_question` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT для таблицы `tests`
+--
+ALTER TABLE `tests`
+  MODIFY `id_test` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
 -- AUTO_INCREMENT для таблицы `users`
 --
 ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- Ограничения внешнего ключа сохраненных таблиц
+--
+
+--
+-- Ограничения внешнего ключа таблицы `answers`
+--
+ALTER TABLE `answers`
+  ADD CONSTRAINT `answers_ibfk_1` FOREIGN KEY (`id_question`) REFERENCES `questions` (`id_question`);
+
+--
+-- Ограничения внешнего ключа таблицы `questions`
+--
+ALTER TABLE `questions`
+  ADD CONSTRAINT `questions_ibfk_1` FOREIGN KEY (`id_test`) REFERENCES `tests` (`id_test`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
